b0038c8c92cd5d58993ee0280ed9ce06b89b9810
Eden <106914220+Zigl3ur@users.noreply.github.com>
2025-04-16 21:30:11 +0200

    feat: added install of meslo font

--- installShell.sh
+++ installShell.sh
@@ -7,7 +7,7 @@ cp configs/config.ghostty ~/.config/ghostty/config
 
 sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-sudo dnf install ripgrep fish zoxide fzf onefetch fastfetch git gh bat eza mpv ghostty -y
+sudo dnf install ripgrep fish zoxide fzf onefetch fastfetch git gh bat eza mpv ghostty meslo-nerd-fonts -y
 curl -sS https://starship.rs/install.sh | sh
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 curl -fsSL https://bun.sh/install | bash
