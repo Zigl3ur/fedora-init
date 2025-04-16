e410c82e95019b7691ff3b7de734379368d9b615
Zigl3ur <eden.douru@hotmail.com>
2025-04-16 19:18:18 +0200

    feat: removed helix

--- installShell.sh
+++ installShell.sh
@@ -1,15 +1,13 @@
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/ghostty
-mkdir -p ~/.config/helix
 
 cp configs/config.fish ~/.config/fish/config.fish
 cp configs/config.starship ~/.config/starship.toml
 cp configs/config.ghostty ~/.config/ghostty/config
-cp configs/config.helix ~/.config/helix/config.toml
 
 sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-sudo dnf install ripgrep fish zoxide fzf helix onefetch ipython fastfetch git gh bat eza mpv ghostty -y
+sudo dnf install ripgrep fish zoxide fzf onefetch fastfetch git gh bat eza mpv ghostty -y
 curl -sS https://starship.rs/install.sh | sh
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 curl -fsSL https://bun.sh/install | bash
